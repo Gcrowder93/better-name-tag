5e70707c7440e6d24c1ec08e9efa2d09d1a4299a
Chase <Gcrowder93@gmail.com>
2021-09-29 11:54:29 -0700

    button works to change value(name)

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import functions and grab DOM elements
 
-// initialize global state
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('first-name');
+//these consts relate to "name" and "generate" IDs in .html
+
+button.addEventListener('click', ()=>{
+  //when button is clicked
+  //get value of input
+  //replace text in nametag w/ value
+  let newName = nameInput.value;
+  firstName.textContent = newName;
+});
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
