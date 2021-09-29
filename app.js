86a5db714ee80abeffd9a685e576881500e46379
Chase <Gcrowder93@gmail.com>
2021-09-29 13:48:12 -0700

    added color buttons

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
-//these consts relate to "name" and "generate" IDs in .html
+const buttoncolor = document.getElementById('change-color');
+const buttoncolor2 = document.getElementById('color-change');
+const header = document.getElementById('head');
+const footer = document.getElementById('foot');
+//these consts relate to "name" and "generate" IDs in .html 
 
 button.addEventListener('click', ()=>{
   //when button is clicked
@@ -11,11 +15,19 @@ button.addEventListener('click', ()=>{
   //replace text in nametag w/ value
     let newName = nameInput.value;
     firstName.textContent = newName;
+
 });
 
+buttoncolor.addEventListener('click', () =>{
+    header.style.backgroundColor = ('#112244');
+    footer.style.backgroundColor = ('#332255'); 
+});
+
+buttoncolor2.addEventListener('click', ()=>{
+    header.style.backgroundColor = ('tomato');
+    footer.style.backgroundColor = ('peachpuff');
+});
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-
-
